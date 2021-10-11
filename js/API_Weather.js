6bd14f63f396759befcec665e600f0262c58aad5
Stanislas Triclin <stanislas.triclin@gmail.com>
2021-10-11 18:38:07 +0200

    Question 4

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -3,9 +3,11 @@
 // Clé api
 const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 // Url API
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+//const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
+
+const API_URL = "https://api.openweathermap.org/data/2.5/forecast/daily";
 
 
 class API_WEATHER{
@@ -22,8 +24,12 @@ class API_WEATHER{
   // Retourne une promise
   fetchTodayForecast(){
     return axios
-    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+    .get(`${API_URL}?q=${this.city}&units=metric&cnt=4&appid=${API_KEY}`, {
       crossdomain: true
+      // API_URL --> adresse à laquelle on envoie la requête
+      // Clé API_KEY --> passe droit pour demander des infos à la page web (API c'est une appli en ligne)
+      // ?--> a partir du point d'interrogation , tu definis des variables qui seront lu par le PHP
+      // q--> détient la ville 
     })
   }
   // Retourne l'element HTML de l'icon symbolisant la méteo.
--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
 // Fonction appelée lors du click du bouton
 function start(){
   // Création de l'objet apiWeather
-  //const city = "";
+  // const city = "";
   const city = document.getElementById("city-input").value;
   console.log(city);
   const apiWeather = new API_WEATHER(city);
+  // créer un objet de type API_Weather que l'on nomme apiweather
   // Appel de la fonction fetchTodayForecast
 
-  apiWeather
-    .fetchTodayForecast()
-    .then(function(response) {
+
+  apiWeather.fetchTodayForecast().then(function(response) {
       // Récupère la donnée d'une API
+      console.log(response);
       const data = response.data;
 
       // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+      
+      const description = data.list[0].weather[0].description;
+      const main = data.list[0].weather[0].main;
+      const temp = data.list[0].temp.day;
+      const icon = apiWeather.getHTMLElementFromIcon(data.list[0].weather[0].icon);
+
+      const description1 = data.list[1].weather[0].description;
+      const main1 = data.list[1].weather[0].main;
+      const temp1 = data.list[1].temp.day;
+      const icon1 = apiWeather.getHTMLElementFromIcon(data.list[1].weather[0].icon);
+
+      const description2 = data.list[2].weather[0].description;
+      const main2 = data.list[2].weather[0].main;
+      const temp2 = data.list[2].temp.day;
+      const icon2 = apiWeather.getHTMLElementFromIcon(data.list[2].weather[0].icon);
+
+      const description3 = data.list[3].weather[0].description;
+      const main3 = data.list[3].weather[0].main;
+      const temp3 = data.list[3].temp.day;
+      const icon3 = apiWeather.getHTMLElementFromIcon(data.list[3].weather[0].icon);
+    
+      console.log(description);
+      console.log(main);
+      console.log(temp);
+      console.log(icon);
 
       // Modifier le DOM
       document.getElementById('today-forecast-main').innerHTML = main;
@@ -25,6 +47,20 @@ function start(){
       document.getElementById('icon-weather-container').innerHTML = icon;
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
       
+      document.getElementById('tomorow1-forecast-main').innerHTML = main1;
+      document.getElementById('tomorow1-forecast-more-info').innerHTML = description1;
+      document.getElementById('icon1-weather-container').innerHTML = icon1;
+      document.getElementById('tomorow1-forecast-temp').innerHTML = `${temp1}°C`;
+      
+      document.getElementById('tomorow2-forecast-main').innerHTML = main2;
+      document.getElementById('tomorow2-forecast-more-info').innerHTML = description2;
+      document.getElementById('icon2-weather-container').innerHTML = icon2;
+      document.getElementById('tomorow2-forecast-temp').innerHTML = `${temp2}°C`;
+
+      document.getElementById('tomorow3-forecast-main').innerHTML = main3;
+      document.getElementById('tomorow3-forecast-more-info').innerHTML = description3;
+      document.getElementById('icon3-weather-container').innerHTML = icon3;
+      document.getElementById('tomorow3-forecast-temp').innerHTML = `${temp3}°C`;
     })
     .catch(function(error) {
       // Affiche une erreur
@@ -32,10 +68,10 @@ function start(){
     });
 }
 
-function getThreeDayForecast(){
+/*function getThreeDayForecast(){
 
   const city = document.getElementById("city-input").value;
   console.log(city);
   const apiWeather = new API_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
-}
+}*/
